5389a2625841550a6358edecf67392c18c221e6b
assembler-creates <chiragjaipal17@gmail.com>
2026-02-10 18:20:04 +0530

    Question 9: Solved

--- src/09-postcard-writer.js
+++ src/09-postcard-writer.js
@@ -52,21 +52,32 @@
  *   countVowels("Namaste")     // => 3
  */
 export function writePostcard(sender, receiver, message) {
-  // Your code here
+  if (typeof sender !== 'string' || typeof receiver !== 'string' || typeof message !== 'string') return "";
+  if (sender.trim() === "" || receiver.trim() === "" || message.trim() === "" ) return "";
+  return `Priy ${receiver},\n\n${message}\n\nAapka/Aapki,\n${sender}`;
 }
 
 export function isValidPincode(code) {
-  // Your code here
+  if (typeof code !== 'string') return false;
+  const regex = /^\d+$/;
+  if (!code.startsWith("0") && code.length === 6 && regex.test(code)) return true;
+  else return false;
 }
 
 export function formatPostcardField(label, value, width) {
-  // Your code here
+  if (typeof label !== 'string' || typeof value !== 'string') return "";
+  if (width !== undefined && typeof width === 'number') return label.padEnd(width) + ": " + value;
+  else return label.padEnd(12) + ": " + value;
 }
 
 export function isFromState(address, stateCode) {
-  // Your code here
+  if (typeof address !== 'string' || typeof stateCode !== 'string') return false;
+  return address.endsWith(stateCode) ? true : false;
 }
 
 export function countVowels(message) {
-  // Your code here
+  if (typeof message !== 'string' || message === "" ) return 0;
+  const result = message.match(/[aeiouAEIOU]/g);
+  if (result === null) return 0;
+  else return result.length;
 }
